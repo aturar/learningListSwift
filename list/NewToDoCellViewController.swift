33446b494bf9942c848fab8ab73ee8d8116238e5
Ayat Turar <aturaru@gmail.com>
2017-09-27 20:27:13 -0400

    Added datepicker, textfield and a separate controller for new ToDo

---
+++ list/NewToDoCellViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewToDoCellViewController.swift
+//  list
+//
+//  Created by Ayat Turar on 2017-09-27.
+//  Copyright © 2017 Ayat Turar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewToDoCellViewController: UITableViewController {
+    
+}
